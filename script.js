2b46c84071f415d2a6e15361e9510be2891cf467
Lisa Cook <jemilox@gmail.com>
2016-08-29 14:13:54 -0500

    added jquery and menu animation

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log('sourced');
+
+// $(document).ready(function(){
+//
+//   $('.one').click(function(){
+//     console.log("meow");
+//     $('.one').animate({width: "20em"});
+//     $('.hiddenMenu').css("display", "block");
+//   });
+
+
+// });
+
+// $(document).ready(function(){
+  var openMenu = function(menuNum){
+    console.log("meow");
+  $(menuNum).animate({width: "20em"});
+
+
+
+};
+
+
+// });
+
+// var openMenu = function(menuNum){
+//   console.log("meow");
+// $("menuNum").toggle(function() {
+//      $(menuNum).animate({width: "20em"});
+//  }, function() {
+//      $(menuNum).animate({width: '5em'});
+//  });
+// };
